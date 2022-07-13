8a9891aeb9f641152be97b0fe02c9972872122dc
Jake Merlin <JacobMerlin92@outlook.com>
2022-07-12 23:27:39 -0400

    First Push

---
+++ Classesexe/Classesexe/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carsexe
+{
+    public class Car
+    {
+        private string _Make = "";
+        private string _Model = "";
+        private int _Year;
+        
+        public static string Make { get; set; }
+        public static string Model { get; set; }
+        public static int Year { get; set; }
+
+    }
+}
---
+++ Classesexe/Classesexe/Program.cs
@@ -0,0 +1,22 @@
+﻿using Carsexe;
+
+namespace Classesexe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            var Model = new Car();
+            var Make = new Car();
+            var Year = new Car();
+
+            Car.Make = "Ford";
+            Car.Model = "Ranger";
+            Car.Year = 2004;
+
+            Console.WriteLine(Car.Make);
+            Console.WriteLine(Car.Model);
+            Console.WriteLine(Car.Year);
+        }
+    }
+}
